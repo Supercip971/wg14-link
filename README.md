cb7a6a7f6e918ea693cec946519f04dfd3e79265
Lynn Kirby <lynn@arrak.is>
2020-01-22 20:02:55 -0500

    Enable redirects
    Adds the initial document metadata and a redirect service. The
    bibliographic details of the documents need to be reviewed before adding
    any feature related to that. Since there are some 2500 documents, that
    process will take some time.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# wg14-link
+# [wg14.link]
+> unofficial ISO C (WG14) redirect service
+
+It's like [wg21.link] except for C and with less features!
+
+## How it works
+
+### Change detection
+
+A hash of the WG14 document log webpage is stored at `data/hash.md5` which is
+checked by a GitHub actions workflow to notify us of any changes.
+
+### Document data
+
+All documents are listed in `data/documents.yml`. Aliases (like `c99`) are
+listed in `data/alias.yml`. These files get parsed at build time by
+`scripts/make-redirect-data.js` to generate a 170 KB JSON data file that our
+redirection endpoint (`api/redirect.js`) consumes. Yes, there is probably a
+better way to store the runtime data.
+
+## License
+
+Released to the public domain under Creative Commons Zero v1.0 Universal.
+
+[wg14.link]: https://wg14.link
+[wg21.link]: https://wg21.link
---
+++ scripts/make-redirect-data.js
@@ -0,0 +1,68 @@
+const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const yaml = require("js-yaml");
+
+// Make things easy by always operating from the project root directory.
+process.chdir(path.join(__dirname, "../"));
+
+const readData = filename => yaml.load(fs.readFileSync(filename), { filename });
+
+const docs = readData("data/documents.yml");
+const alias = readData("data/alias.yml");
+
+// Canonicalize document ID to n# without leading zeroes.
+const canonicalDocumentId = id => {
+  assert(id.match("^N[0-9]+$"));
+  const n = Number.parseInt(id.substr(1), 10);
+  return "n" + n;
+};
+
+const redirects = {};
+
+// Add all of the document files.
+for (let { id, url, status } of docs) {
+  const redirect = {};
+
+  id = canonicalDocumentId(id);
+
+  // Default status when not provided.
+  if (status === undefined) {
+    status = "found";
+  }
+
+  // Figure out the URL and action based on the document status.
+  if (status === "missing") {
+    // Document file is missing (probably forever).
+    redirect.error = true;
+    redirect.url = "/404-missing.html";
+  } else if (status === "unassigned") {
+    // ID is unassigned.
+    redirect.error = true;
+    redirect.url = "/404-unassigned.html";
+  } else {
+    // Document file is found.
+    assert((status === "found") || (status === "protected"));
+    redirect.url = url;
+  }
+
+  assert(redirects[id] === undefined, `duplicate ID found: ${id}`);
+  redirects[id] = redirect;
+}
+
+// Add the aliases.
+for (let [from, to] of Object.entries(alias)) {
+  from = from.toLowerCase();
+  to = canonicalDocumentId(to);
+  const redirect = redirects[to];
+  assert(redirect !== undefined);
+  assert(redirect.url !== undefined);
+  assert(redirects[from] === undefined);
+  redirects[from] = { url: redirect.url };
+}
+
+// Write the redirect file.
+fs.mkdirSync("build");
+fs.writeFileSync("build/redirect.json", JSON.stringify(redirects));
+console.log("build/redirect.json has been written");
